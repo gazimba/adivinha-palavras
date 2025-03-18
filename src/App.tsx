4e133e0a391f01d32a47f05a7b5584da331da468
gazimba <gabrielb3r@gmail.com>
2025-03-18 16:49:24 -0300

    criando componente letter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './global.css';
 import { Header } from "./components/Header";
 import styles from './app.module.css';
+import { Tips } from './components/Tip';
+import { Letter } from './components/Letter';
 
 export function App() {
     function handleRestartGame() {
@@ -10,6 +12,8 @@ export function App() {
         <div className={styles.container}>
             <main>
                 <Header current={5} max={10} onRestart={handleRestartGame} />
+                <Tips tip="Uma das linguagens de programação mais usadas." />
+                <Letter value="J" />
             </main>
         </div>
     )
---
+++ src/components/Tip/index.tsx
@@ -0,0 +1,18 @@
+import styles from './tip.module.css';
+import tipIcon from '../../assets/tip.svg';
+
+type Props = {
+    tip: string;
+}
+
+export function Tips({tip}: Props) {
+    return (
+        <div className={styles.tip}>
+            <img src={tipIcon} alt="Icone da dica"  />
+            <div>
+                <h3>Dica</h3>
+                <p>{tip}</p>
+            </div>
+        </div>
+    )
+}
